2d47b1c75cbf841c61fd75f6fb88b620c58b2d3e
Tommy Grullon <tommycontreras34@gmail.com>
2026-06-23 16:28:10 -0400

    test: add unit tests for refresh token controller with token regeneration logic

---
+++ src/tests/controllers/auth/refresh-token.controller.spec.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect, vi, afterEach } from 'vitest';
+import request from 'supertest';
+import app from '../../../app.js';
+import { StatusCode } from '../../../constants/status-code.js';
+import * as refreshTokenService from '../../../services/auth/refresh-token.service.js';
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from '../../../auth/jwt.js';
+
+describe('POST /refresh-token (controller)', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it('should regenerate a token', async () => {
+    const refreshToken = generateRefreshToken({
+      userId: 1,
+      email: 'john@gmail.com',
+    });
+    vi.spyOn(refreshTokenService, 'refreshTokenService').mockResolvedValue(
+      refreshToken,
+    );
+
+    const token = generateAccessToken({ userId: 1, email: 'john@gmail.com' });
+
+    const response = await request(app)
+      .post('/refresh-token')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        refreshToken: 'fake-refresh-token',
+      });
+
+    expect(response.status).toBe(StatusCode.OK);
+
+    expect(response.body.token).toBe(refreshToken);
+  });
+});
